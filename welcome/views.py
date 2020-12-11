36430cd2ca1424122b88eb5faeea18b2b3d572ff
William <wgaertner@my.waketech.edu>
2020-12-10 19:44:00 -0500

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -9,24 +9,7 @@ from .models import PageView
 
 # Create your views here.
 
-def index(request):
-    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
-    return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
-
-def health(request):
-    """Takes an request as a parameter and gives the count of pageview objects as reponse"""
-    return HttpResponse(PageView.objects.count())
-
 def image_view(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
 
     if request.method == 'POST': 
         form = ImageForm(request.POST, request.FILES) 
